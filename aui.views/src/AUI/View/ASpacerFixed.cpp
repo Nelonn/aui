3566d081e495b2e579ae6b9ab6d2e3a0ffa88918
alex2772 <alex2772sc@gmail.com>
2026-09-06 05:02:16 +0300

    fix spacer fixed

--- aui.views/src/AUI/View/ASpacerFixed.cpp
+++ aui.views/src/AUI/View/ASpacerFixed.cpp
@@ -11,37 +11,40 @@
 
 #include "ASpacerFixed.h"
 
-AMinMaxAxis ASpacerFixed::onComputeIntrinsicMinMaxAxis(int height) {
-    if (auto parent = getParent()) {
-        if (const auto& layout = parent->getLayout()) {
-            if (layout->getLayoutDirection() == ALayoutDirection::HORIZONTAL) {
-                return {
-                    .min = static_cast<int>(mSpace.getValuePx()),
-                    .max = static_cast<int>(mSpace.getValuePx()),
-                };
-            }
-        }
-    }
-    return {};
-}
-
 bool ASpacerFixed::consumesClick(const glm::ivec2& pos) {
     return false;
 }
 
 IInspectable::DebugInspectorInfo ASpacerFixed::debugInspectorInfo() {
     auto info = AView::debugInspectorInfo();
-    info.push_back({ "space", mSpace });
+    info.push_back({"space", mSpace});
     auto parent = getParent();
     if (!parent) {
-        info.push_back({ "direction", "<no parent>" });
+        info.push_back({"direction", "<no parent>"});
         return info;
     }
     const auto& layout = parent->getLayout();
     if (!layout) {
-        info.push_back({ "direction", "<no layout>" });
+        info.push_back({"direction", "<no layout>"});
         return info;
     }
-    info.push_back({ "direction", layout->getLayoutDirection() });
+    info.push_back({"direction", layout->getLayoutDirection()});
     return info;
+}
+
+glm::ivec2 ASpacerFixed::onIntrinsicMeasure(AConstraints constraints) {
+    if (auto parent = getParent()) {
+        if (const auto& layout = parent->getLayout()) {
+            switch (layout->getLayoutDirection()) {
+            case ALayoutDirection::HORIZONTAL:
+                return glm::ivec2(mSpace.getValuePx(), 0);
+            case ALayoutDirection::VERTICAL:
+                return glm::ivec2(0, mSpace.getValuePx());
+
+            case ALayoutDirection::NONE:
+                break;
+            }
+        }
+    }
+    return AView::onIntrinsicMeasure(constraints);
 }
